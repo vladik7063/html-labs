b2706a72c88de08f7bc77b26f364d67ae02f6b23
vladik7063 <40918996+vladik7063@users.noreply.github.com>
2022-03-16 19:40:07 +0300

    Create script.js

---
+++ lab3.4/script.js
@@ -0,0 +1,17 @@
+function myFunc() {
+    document.querySelector('#key').style.background = document.querySelector('#key1').style.color;
+    }
+    
+    function myFunc1() {
+    document.querySelector('#key').style.background = document.querySelector('#key2').style.color;
+    }
+    
+    function myFunc2() {
+    document.querySelector('#key').style.background = document.querySelector('#key3').style.color;
+    }
+    function myFunc3() {
+    document.querySelector('#key').style.background = document.querySelector('#key4').style.color;
+    }
+    function myFunc4() {
+    document.querySelector('#key').style.background = document.querySelector('#key5').style.color;
+    }
